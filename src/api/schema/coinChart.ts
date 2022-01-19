325cfa3f4ae4899c872278df1a1a19b3410b9e07
WJ951009 <fsdjin159@gmail.com>
2022-01-20 03:25:45 +0900

    feat: Create Chart component using Apex-Chart

---
+++ src/api/schema/coinChart.ts
@@ -0,0 +1,10 @@
+export interface CoinChartSchema {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
