d29195f4a7fff765766242004a8d28c48fcf7002
WJ951009 <fsdjin159@gmail.com>
2022-01-20 03:33:37 +0900

    refactor: Delete unuse elements of Chart Interface

--- src/api/schema/coinChart.ts
+++ src/api/schema/coinChart.ts
@@ -1,10 +1,7 @@
 export interface CoinChartSchema {
-  time_open: string;
   time_close: string;
   open: number;
   high: number;
   low: number;
   close: number;
-  volume: number;
-  market_cap: number;
 }
