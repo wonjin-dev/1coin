b5d2f1cfa6ccb15954b843b86db7353f18dc4f88
WJ951009 <fsdjin159@gmail.com>
2022-01-20 01:54:02 +0900

    feat: Mapping data for Coin component from API

--- src/api/coin.ts
+++ src/api/coin.ts
@@ -5,3 +5,15 @@ export const getCoinList = async() => {
   let {data} = await axios.get(url);
   return data;
 }
+
+export const getCoinDetails = async(coinId: string) => {
+  let url = `https://api.coinpaprika.com/v1/coins/${coinId}`;
+  let {data} = await axios.get(url);
+  return data;
+}
+
+export const getCoinTickers = async(coinId: string) => {
+  let url = `https://api.coinpaprika.com/v1/tickers/${coinId}`;
+  let {data} = await axios.get(url);
+  return data;
+}
--- src/pages/Coin.tsx
+++ src/pages/Coin.tsx
@@ -1,8 +1,48 @@
+import {useParams} from "react-router-dom";
+import {useQuery} from "react-query";
+import {CoinDetailsSchema} from "../api/schema/coinDetails";
+import {CoinTickerSchema} from "../api/schema/coinTicker";
+import {getCoinDetails, getCoinTickers} from "../api/coin";
+
+interface RouteParams {
+  coinId: string;
+}
+
 const Coin = () => {
+  const {coinId} = useParams<RouteParams>();
+  const {isLoading: infoLoading, data: infoData} = useQuery<CoinDetailsSchema>(
+    ["details", coinId],
+    () => getCoinDetails(coinId)
+  );
+  const {isLoading: tickersLoading, data: tickersData} = useQuery<CoinTickerSchema>(
+    ["tickers", coinId],
+    () => getCoinTickers(coinId)
+  );
+  const loading = infoLoading || tickersLoading;
   return (
     <>
+      {loading
+        ? (<span>Loading...</span>)
+        : (<>
+          <div>
+            <span>Rank:</span>
+            <span>{infoData?.rank}</span>
+          </div>
+          <div>
+            <span>Symbol:</span>
+            <span>${infoData?.symbol}</span>
+          </div>
+          <div>
+            <span>Price:</span>
+            <span>${tickersData?.quotes.USD.price.toFixed(1)}</span>
+          </div>
+          <div>
+            <p>{infoData?.description}</p>
+          </div>
+        </>)
+      }
     </>
-  )
+  );
 }
 
 export default Coin;
