b109f26a62ac497d9c967ae1da8e4718e6a30614
WJ951009 <fsdjin159@gmail.com>
2022-01-19 20:00:32 +0900

    feat: Set React-Query & Get data of Coinlist from API

--- src/pages/Coins.tsx
+++ src/pages/Coins.tsx
@@ -1,8 +1,19 @@
+import {useQuery} from "react-query";
+import {CoinListSchema} from "../api/schema/coinList";
+import {getCoinList} from "../api/coin";
+
 const Coins = () => {
+  const {data} = useQuery<CoinListSchema[]>("Coins", getCoinList);
   return (
     <>
+      {data?.map((coin: CoinListSchema) => (
+        <div key={coin.id}>
+          <img src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
+          {coin.name}
+        </div>
+      ))}
     </>
   )
 }
-
+  
 export default Coins;
