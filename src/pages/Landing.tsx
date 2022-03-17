9c913c7dd982f5a509118cc5201b4ad9af7da65e
wonjin-dev <fsdjin159@gmail.com>
2022-03-18 00:18:08 +0900

    feat: Create register page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,12 +4,14 @@ import {Link} from 'react-router-dom';
 import {STRINGS} from '../constants/ko';
 import {userType} from '../types';
 import PublicInput from '../components/PublicInput';
+import PublicBtn from '../components/PublicBtn';
 
 const Landing = () => {
   const [user, setUser] = useState<userType>({
     id: undefined,
     pw: undefined
   });
+  
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUser({
       ...user,
@@ -19,37 +21,24 @@ const Landing = () => {
 
   return (
     <Conatiner>
-      <Content>
-        <form
-          method='post'
-          // action=''
-        >
-          <PublicInput
-            name={'id'}
-            onChange={() => onChange}
-          />
-          <PublicInput
-            type={'password'}
-            name={'pw'}
-            onChange={() => onChange}
-          />
-          <SubmitBtn>
-            {STRINGS.login}
-          </SubmitBtn>
-        </form>
-        <ButtonContainer>
-          <Link to={'/coins'}>
-            <SubmitBtn>
-              {STRINGS.review}
-            </SubmitBtn>
-          </Link>
-          <Link to={'/register'}>
-            <SubmitBtn>
-              {STRINGS.register}
-            </SubmitBtn>
-          </Link>
-        </ButtonContainer>
-      </Content>
+      <PublicInput
+        name={'id'}
+        onChange={() => onChange}
+      />
+      <PublicInput
+        type={'password'}
+        name={'pw'}
+        onChange={() => onChange}
+      />
+      <PublicBtn value={STRINGS.login} />
+      <ButtonContainer>
+        <Link to={'/coins'}>
+          <PublicBtn value={STRINGS.review} />
+        </Link>
+        <Link to={'/register'}>
+          <PublicBtn value={STRINGS.register} />
+        </Link>
+      </ButtonContainer>
     </Conatiner>
   )
 }
@@ -64,16 +53,7 @@ const Conatiner = styled.div`
   height: 100vh;
 `
 
-const Content = styled.div`
-  flex-direction: column;
-`
-
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: center;
-`
-
-const SubmitBtn = styled.button`
-  width: 80px;
-  height: 30px;
 `
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,47 @@
+import {useState} from 'react';
+import {STRINGS} from '../constants/ko';
+import PublicBtn from '../components/PublicBtn';
+import PublicInput from '../components/PublicInput';
+
 const Register = () => {
+  const [userInfo, setUserInfo]= useState({
+    email: undefined,
+    id: undefined,
+    pw: undefined
+  });
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserInfo({
+      ...userInfo,
+      [e.target.name]: e.target.value
+    });
+  };
+  
+  const onClickRegister = () => {
+    console.log('스테이트를 로컬 스토리지에 보내야 함');
+  }
+  
   return (
     <>
+      <PublicInput
+        name={'email'}
+        value={userInfo.email}
+        onChange={() => onChange}
+      />
+      <PublicInput
+        name={'id'}
+        value={userInfo.id}
+        onChange={() => onChange}
+      />
+      <PublicInput
+        name={'pw'}
+        value={userInfo.pw}
+        onChange={() => onChange}
+      />
+      <PublicBtn
+        value={STRINGS.register}
+        onClick={() => onClickRegister}
+      />
     </>
   )
 }
