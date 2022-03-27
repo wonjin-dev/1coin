3f5ef3949b311ba9fae7e1d3afca5694d14d84ce
wonjin-dev <fsdjin159@gmail.com>
2022-03-27 18:46:36 +0900

    refactor: Improve util function of checking overlap

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -7,7 +7,7 @@ import {STRINGS} from '../constants/ko';
 import PublicBtn from '../components/PublicBtn';
 import PublicInput from '../components/PublicInput';
 import {userType} from '../types';
-import checkOverlap from '../utils/checkOverlap';
+import {checkOverlap} from '../utils/checkOverlap';
 const Register = () => {
   const [newUser, setNewUser] = useState<userType>({
     email: '',
@@ -24,9 +24,17 @@ const Register = () => {
   };
   
   const onClickRegister = () => {
-    const overlapBool = checkOverlap(users, newUser.id);
+    const overlapBool = checkOverlap({
+      origin: users,
+      key: 'id',
+      new: newUser.id || ''
+    });
+    
     if(overlapBool === false) {
       setUsers([...users, newUser]);
+      // TODO:: input 유효성 검사 추가
+      // TODO:: 회원가입 성공시 랜딩페이지로 라우팅
+      // TODO:: 모달 추가하기 => '바로 로그인', '로그인 창으로'
     } else {
       // TODO:: 모달 추가하기
       throw new Error(STRINGS.overlapId);
--- src/utils/checkOverlap.ts
+++ src/utils/checkOverlap.ts
@@ -1,16 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const checkOverlap = (origin: any, key?: string): boolean => {
-// eslint-disable-next-line prefer-const
-  let origins = [];
-  for(let i=0; i<origin.length; i++){
-    origins.push(origin[i].id);
-  }
-  const check = origins.indexOf(key);
-  if(check < 0){
-    return false;
-  } else {
-    return true;
-  }
+export const checkOverlap = <T extends any[]>(
+  arg: {
+    origin: T;
+    new: string;
+    key: string;
+  },
+): boolean => {
+  const result = arg.origin.some(item => item[arg.key] === arg.new);
+  return result
 }
-
-export default checkOverlap;
