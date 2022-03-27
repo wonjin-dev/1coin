2b64a22c2cf9e681a1b1547660b39c819ac9104f
wonjin-dev <fsdjin159@gmail.com>
2022-03-27 18:53:28 +0900

    [WIP]: To Do List 정리 (라우팅, 커스텀 모달)

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,8 @@
 import {useState} from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
+import {useRecoilState, useRecoilValue} from 'recoil';
+import {userAtom, authAtom} from '../atoms';
 import {STRINGS} from '../constants/ko';
 import {userType} from '../types';
 import PublicInput from '../components/PublicInput';
@@ -11,7 +13,8 @@ const Landing = () => {
     id: undefined,
     pw: undefined
   });
-  
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [auth, setAuth] = useRecoilState(authAtom);
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUser({
       ...user,
@@ -20,21 +23,33 @@ const Landing = () => {
   };
 
   const onClickLogin = () => {
-    //
+    const origin = useRecoilValue(userAtom);
+    for(let i=0; i<origin.length; i++){
+      if(origin[i].id === user.id){
+        if(origin[i].pw === user.pw){
+          setAuth(true)
+          // TODO:: Home화면으로 라우팅
+        } else {
+          // 비밀번호가 틀립니다
+        } 
+      } else {
+        // 일치하는 아이디가 없습니다
+      }
+    }
   };
 
   return (
     <Conatiner>
       <PublicInput
         name={'id'}
-        onChange={() => onChange}
+        onChange={onChange}
       />
       <PublicInput
         type={'password'}
         name={'pw'}
-        onChange={() => onChange}
+        onChange={onChange}
       />
-      <PublicBtn value={STRINGS.login} onClick={() => onClickLogin} />
+      <PublicBtn value={STRINGS.login} onClick={onClickLogin} />
       <Link to={'/coins'}>
         <PublicBtn value={STRINGS.review} />
       </Link>
