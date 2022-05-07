836b6107e5885994697a06d81bd09bed0019eccf
wonjin-dev <fsdjin159@gmail.com>
2022-05-08 00:56:15 +0900

    feat: Add English STRINGS

---
+++ src/constants/en.ts
@@ -0,0 +1,15 @@
+export const STRINGS = {
+  pjName: '1COIN',
+  pjTitle: 'Choice your 1COIN',
+  loadCoinList: 'Getting list of coins . . .',
+  loadCoinDetails: 'Retrieving that coin information . . .',
+  rank: 'rank: ',
+  price: 'price: ',
+  seeChart: 'See Chart',
+  seeTheStars: 'See stared Coins',
+  noStaredCoin: 'There are no stared coins. Please click the star-icon next to the card to add it : )',
+  seeAll: 'See All',
+  darkTheme: 'Dark Theme',
+  ligthThme: 'Light Theme',
+  back: 'back'
+};
